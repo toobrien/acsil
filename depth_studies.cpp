6070efb6b9f5f360897fc61adc2140fd7bbafdcc
Taylor O'Brien <toobrien@amazon.com>
2022-06-09 19:10:37 -0700

    added delta, volume

--- depth_studies.cpp
+++ depth_studies.cpp
@@ -37,20 +37,23 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 	#define delta_row				3
 	#define volume_row				4
 
-	#define base_row_key	0
-	#define num_trades_key	1
-	#define trades_key		2
-	#define trades_idx_key	3
+	#define base_row_key			0
+	#define num_trades_key			1
+	#define tas_idx_key				2
+	#define at_bid_total_key		3
+	#define at_ask_total_key		4
+	#define high_volume_key			5
 
 	// set defaults
 	
 	SCInputRef symbol_input	= sc.Input[0];
 
-	void * 	h			= sc.GetSpreadsheetSheetHandleByName("depth_sheet", "main", false);
-	int	&	base_row 	= sc.GetPersistentInt(base_row_key);
-	int &	num_trades  = sc.GetPersistentInt(num_trades_key);
-	int *   trades 		= static_cast<int *>(sc.GetPersistentPointer(trades_key));
-	int &	trades_idx	= sc.GetPersistentInt(trades_idx_key);
+	void * 	h				= sc.GetSpreadsheetSheetHandleByName("depth_sheet", "main", false);
+	int	&	base_row 		= sc.GetPersistentInt(base_row_key);
+	int &	tas_idx			= sc.GetPersistentInt(tas_idx_key);
+	int & 	at_bid_total	= sc.GetPersistentInt(at_bid_total_key);
+	int & 	at_ask_total	= sc.GetPersistentInt(at_ask_total_key);
+	int &	high_volume		= sc.GetPersistentInt(high_volume_key);
 
 	if (sc.SetDefaults) {
 
@@ -62,8 +65,11 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 		symbol_input.Name = "symbol";
 		symbol_input.SetString("");
 		
-		trades 		= NULL;
-		base_row 	= -1;
+		base_row 		= -1;
+		tas_idx			= -1;
+		at_bid_total	= -1;
+		at_ask_total	= -1;
+		high_volume		= -1;
 
 		return;
 		
@@ -96,40 +102,89 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 
 	}
 
-	// initialize remaining inputs from spreadsheet
+	// START	initialize remaining inputs from spreadsheet
 
-	double d_max_outputs 	= -1;
-	double d_threshold 		= -1;
-	double d_alert_distance = -1;
-	double d_num_trades_	= -1;
-	double d_num_liq_levels = -1;
+	double d_max_outputs 	= -1.0;
+	double d_threshold 		= -1.0;
+	double d_alert_distance = -1.0;
+	double d_num_trades		= -1.0;
+	double d_num_liq_lvls 	= -1.0;
 	
 	sc.GetSheetCellAsDouble(h, input_val_col, base_row + max_outputs_row, d_max_outputs);
 	sc.GetSheetCellAsDouble(h, input_val_col, base_row + threshold_row, d_threshold);
 	sc.GetSheetCellAsDouble(h, input_val_col, base_row + alert_distance_row, d_alert_distance);
-	sc.GetSheetCellAsDouble(h, input_val_col, base_row + num_trades_row, d_num_trades_);
-	sc.GetSheetCellAsDouble(h, input_val_col, base_row + num_trades_row, d_num_liq_levels);
+	sc.GetSheetCellAsDouble(h, input_val_col, base_row + num_trades_row, d_num_trades);
+	sc.GetSheetCellAsDouble(h, input_val_col, base_row + num_liq_lvls_row, d_num_liq_lvls);
 
 	int max_outputs 	= static_cast<int>(d_max_outputs);
 	int threshold		= static_cast<int>(d_threshold);
 	int alert_distance	= static_cast<int>(d_alert_distance);
-	int num_trades_		= static_cast<int>(d_num_trades_);
-	int num_liq_levels  = static_cast<int>(d_num_liq_levels);
+	int num_trades		= static_cast<int>(d_num_trades);
+	int num_liq_levels  = static_cast<int>(d_num_liq_lvls);
 
-	// first run, or user has changed number of trades: re-allocate buffer
+	c_SCTimeAndSalesArray tas;
 
-	if (num_trades_ != num_trades) {
+	if (num_trades > 0)
 
-		num_trades 	= num_trades;
-		trades_idx 	= 0;
-		delete trades;
-		trades		= new int[num_trades];
+		sc.GetTimeAndSales(tas);
+
+	// END		initialize remaining inputs from spreadsheet
+
+	// START 	time and sales: delta, volume
+
+	int 	len_tas	= tas.Size();
+	float 	at_bid	= 0.0;
+	float 	at_ask	= 0.0;
+	float 	delta 	= -1.0;
+	float 	volume	= 0.0;
+
+	if (len_tas > 0) {
+
+		int count	= 0;
+		int j 		= len_tas - min(num_trades, len_tas);
+
+		for (int i = j; i < len_tas; i++) {
+
+			s_TimeAndSales r = tas[i];
+			r *= sc.RealTimePriceMultiplier;
+
+			if (r.Type == SC_TS_BID) {
+
+				at_bid += r.Volume;
+
+				count++;
+				// sc.AddMessageToLog(("at_bid: " + std::to_string(at_bid)).c_str(), 1);
+
+			} else if (r.Type == SC_TS_ASK) {
+
+				at_ask += r.Volume;
+				
+				count++;
+				// sc.AddMessageToLog(("at_ask" + std::to_string(at_ask)).c_str(), 1);
+
+			}
+
+		}
+
+		// sc.AddMessageToLog(("total trades" + std::to_string(count)).c_str(), 1);
+
+		if (at_bid > 0.0)
+		
+			delta = at_ask / at_bid;
+
+		volume = at_bid + at_ask;
+
+		if (volume > high_volume)
+
+			high_volume = volume;
+
+		volume /= high_volume;
 
 	}
 
-	// sc.AddMessageToLog(("max_outputs: " + std::to_string(max_outputs)).c_str(), 1);
-	// sc.AddMessageToLog(("threshold: " + std::to_string(threshold)).c_str(), 1);
-	// sc.AddMessageToLog(("alert_distance: " + std::to_string(alert_distance)).c_str(), 1);
+	// END		time and sales: delta, volume
+
+	// START	large order levels, liquidity balance, from high/low
 
 	if (threshold <= 0 || max_outputs <= 0)
 
@@ -151,8 +206,8 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 	int len_bids = 0;
 	int len_asks = 0;
 
-	int from_high	= -1;
-	int from_low	= -1;
+	int from_high 	= -1;
+	int from_low 	= -1;
 
 	int 	total_bids 			= 0;
 	int 	total_asks 			= 0;
@@ -225,9 +280,11 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 
 	}
 
-	// calculate statistics
-
 	liquidity_balance = static_cast<float>(total_asks) / total_bids;
+
+	// END		large order levels, liquidity balance, from high/low
+
+	// START	spreadsheet
 
 	// clear spreadsheet
 
@@ -279,7 +336,7 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 
 	}
 
-	// set from high / low
+	// set stats
 
 	if (from_high > 0)
 
@@ -290,16 +347,18 @@ SCSFExport scsf_large_orders(SCStudyInterfaceRef sc) {
 		sc.SetSheetCellAsDouble(h, stat_val_col, base_row + from_low_row, from_low);
 
 
-	// set liquidity balance
-
 	if (liquidity_balance > 0.0)
 
 		sc.SetSheetCellAsDouble(h, stat_val_col, base_row + liquidity_balance_row, liquidity_balance);
 
-	// clean up trades array
+	if (delta > 0.0)
 
-	 if (sc.LastCallToFunction)
+		sc.SetSheetCellAsDouble(h, stat_val_col, base_row + delta_row, delta);
 
-		delete trades;
+	if (volume > 0)
+
+		sc.SetSheetCellAsDouble(h, stat_val_col, base_row + volume_row, volume);
+
+	// END		spreadsheet
 
 }
